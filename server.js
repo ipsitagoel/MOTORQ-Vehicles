a5f6608debcd47f8b6428cd23f2bdb8dc6b446f7
ipsitagoel <ipsita.goel1906@gmail.com>
2021-10-12 02:33:13 +0530

    initial commit

--- server.js
+++ server.js
@@ -94,7 +94,12 @@ app.patch('/vehicles/:id', (req,res) => {
 })
 
 if(process.env.NODE_ENV == 'production'){
-    app.use(express.static("client/build"));
+    app.use(express.static("./client/build"));
+    const path=require("path");
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+})
 }
 
 app.listen(process.env.PORT || 5000, () => {
