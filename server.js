5b8314debd98897b54d9d03532b01d6eb98abcb3
ipsitagoel <ipsita.goel1906@gmail.com>
2021-10-12 02:42:11 +0530

    initial commit

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.patch('/vehicles/:id', (req,res) => {
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("./client/build"));
-    const path=require("path");
+    import path from "path";
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')
     );
