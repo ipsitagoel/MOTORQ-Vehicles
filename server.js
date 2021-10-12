4d81e19027364530ae58755d506858a051de0efd
ipsitagoel <ipsita.goel1906@gmail.com>
2021-10-12 15:23:42 +0530

    init

--- server.js
+++ server.js
@@ -98,10 +98,13 @@ app.patch('/api/vehicles/:id', (req,res) => {
 
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static("client/build"));
-//     app.get('*',(req,res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')
-//     );
-// })
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 }
 
 app.listen(process.env.PORT || 5000, () => {
